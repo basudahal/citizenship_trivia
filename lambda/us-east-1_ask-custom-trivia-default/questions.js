98d0d92d9b8657ca5df98d0535896ea5a8ece433
System Administrator <root@basudevs-air.lan>
2020-11-28 12:49:07 -0700

    first commit

---
+++ lambda/us-east-1_ask-custom-trivia-default/questions.js
@@ -0,0 +1,778 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'What is the supreme law of the land?': [
+        'the Constitution',
+        'the Law',
+        'the Supreme Court',
+        'the Congress',
+      ],
+    },
+    {
+      'What does the Constitution do?': [
+        'Sets up Government',
+        'Protects People',
+        'Defines the Law',
+        'Reviews the Law',
+      ],
+    },
+    {
+      'The idea of self-government is in the first three words of the Constitution. What are these words?': [
+        'We the People',
+        'We the American',
+        'the American People',
+        'People of America',
+      ],
+    },
+    {
+      'What is an amendment? ': [
+        'A change/addition to the constitution',
+        'a change to the government',
+        'a change to the legislation',
+        'an addition to the government',
+      ],
+    },
+    {
+      'What do we call the first ten amendments to the Constitution?': [
+        'the Bill of Rights',
+        'the Bill of Amendments',
+        'the Bill of Powers',
+        'the Bill of Government',
+      ],
+    },
+    {
+      'Which one of the following is not one right or freedom from the First Amendment?': [
+        'Assembly',
+        'Respect',
+        'Self Defense',
+        'Travel',
+      ],
+    },
+    {
+      'How many amendments does the Constitution have?': [
+        '27',
+        '10',
+        '32',
+        '9',
+      ],
+    },
+    {
+      'What did the Declaration of Independence do?': [
+        'Announced our freedom from Great Britain',
+        'Protects American People',
+        'Declares a war',
+        'Prints Money',
+      ],
+    },
+    {
+      'What are rights in the Declaration of Independence?': [
+        'life, liberty and persuit of happiness',
+        'life, freedom and assembly ',
+        'liberty, democracy and speech',
+        'Speech, freedom and persuit of happiness',
+      ],
+    },
+    {
+      'What is freedom of religion?': [
+        'You can practice any religion, or not practice a religion ',
+        'You can practice one religion',
+        'You can\'t practice any religion',
+        'Religion can be avoided',
+      ],
+    },
+    {
+      'What is the economic system in the United States?': [
+        'Capitalist Economy',
+        'Traditional Economy',
+        'Mixed Economy',
+        'Socialist Economy',
+      ],
+    },
+    {
+      'What is the “rule of law”?': [
+        'No one is above the law',
+        'Everyone must not follow the law',
+        'Some one may break the law',
+        'President is above the law',
+      ],
+    },
+    {
+      'What is one branch or part of the government?': [
+        'Congress',
+        'Federal',
+        'States',
+        'FBI',
+      ],
+    },
+    {
+      'What stops one branch of government from becoming too powerful?': [
+        'Checks and Balances',
+        'Powers and Balances',
+        'Checks and Powers',
+        'the Supreme Court judge',
+      ],
+    },
+    {
+      'Who is in charge of the executive branch?': [
+        'the President',
+        'Supreme Court Judge',
+        'the Speaker of the House',
+        'the Vice President',
+      ],
+    },
+    {
+      'Who makes federal laws?': [
+        'Congress',
+        'Senate',
+        'House',
+        'Supreme Court',
+      ],
+    },
+    {
+      'What are the two parts of the U.S. Congress?': [
+        'Senate and House',
+        'the President and the Vice President',
+        'Federal and States',
+        'House and Legislation',
+      ],
+    },
+    {
+      'How many U.S. Senators are there?': [
+        '100',
+        '435',
+        '535',
+        '350',
+      ],
+    },
+    {
+      'We elect a U.S. Senator for how many years?': [
+        '6',
+        '4',
+        '5',
+        '2',
+      ],
+    },
+    {
+      'The House of Representatives has how many voting members?': [
+        '435',
+        '290',
+        '535',
+        '100',
+      ],
+    },
+    {
+      'We elect a U.S. Representative for how many years?': [
+        '2',
+        '4',
+        '6',
+        '5',
+      ],
+    },
+    {
+      'Who does a U.S. Senator represent?': [
+        'all people of the state',
+        'the President',
+        'the Governor',
+        'Political Party',
+      ],
+    },
+    {
+      'Why do some states have more Representatives than other states?': [
+        'Because of the population of the state',
+        'Because of the area of the state',
+        'Because of the majority in house',
+        'Because of total number of house seats',
+      ],
+    },
+    {
+      'We elect a President for how many years?': [
+        '4',
+        '5',
+        '2',
+        '6',
+      ],
+    },
+    {
+      'In what month do we vote for President?': [
+        'November',
+        'January',
+        'October',
+        'December',
+      ],
+    },
+    {
+      'What is the name of the President of the United States now?': [
+        'Donald Trump',
+        'George W Bush',
+        'Bill Clinton',
+        'Barack Obama',
+      ],
+    },
+    {
+      'What is the name of the Vice President of the United States now?': [
+        'Mike Pence',
+        'Joe Biden',
+        'Al Gore',
+        'Tim Kaine',
+      ],
+    },
+    {
+      'If the President can no longer serve, who becomes President?': [
+        'the Vice President',
+        'the Speaker of the House',
+        'the First Lady',
+        'No one until the next Election',
+      ],
+    },
+    {
+      'If both the President and the Vice President can no longer serve, who becomes President?': [
+        'the Speaker of the House',
+        'the Vice President',
+        'the First Lady',
+        'No one until the next Election',
+      ],
+    },
+    {
+      'Who is the Commander in Chief of the military?': [
+        'the President',
+        'the Secretary of Defense',
+        'the Vice President',
+        'the Attorney General',
+      ],
+    },
+    {
+      'Who signs bills to become laws?': [
+        'the President',
+        'the Chief Justice',
+        'the Vice President',
+        'the Speaker of the House',
+      ],
+    },
+    {
+      'Who vetoes bills?': [
+        'the President',
+        'the Chief Justice',
+        'the Vice President',
+        'the Speaker of the House',
+      ],
+    },
+    {
+      'What does the President’s Cabinet do?': [
+        'advises the President',
+        'reviews the Law',
+        'explains the Law',
+        'briefs the press meetings',
+      ],
+    },
+    {
+      'Which one is not Cabinet-level positions?': [
+        'Secretary of Technology',
+        'Secretary of Agriculture',
+        'Secretary of Education',
+        'Secretary of Defense',
+      ],
+    },
+    {
+      'What does the judicial branch do?': [
+        'reviews and explains the Law',
+        'advises the President',
+        'makes the Law',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What is the highest court in the United States?': [
+        'the Supreme Court',
+        'the Federal Court',
+        'the State Court',
+        'the District Court',
+      ],
+    },
+    {
+      'How many justices are on the Supreme Court?': [
+        '9',
+        '10',
+        '27',
+        '12',
+      ],
+    },
+    {
+      'Who is the Chief Justice of the United States now?': [
+        'John Roberts',
+        'Neil Goresuch',
+        'Brett Kavannaugh',
+        'Bernie Sanders',
+      ],
+    },
+    {
+      'Under our Constitution, some powers belong to the federal government. What is one power of the federal government?': [
+        'declares war',
+        'issues the drivers license',
+        'funds health and education',
+        'provides police protection',
+      ],
+    },
+    {
+      'Under our Constitution, some powers belong to the states. What is one power of the states?': [
+        'issues the drivers license',
+        'prints money',
+        'declares war',
+        'protects the wildlife',
+      ],
+    },
+    {
+      'If you live in Disctrict of Columbia, Who is the Governor of your state now?': [
+        'D.C. Doesn\'t have a Governor',
+        'the President',
+        'the Vice President',
+        'Don\'t know',
+      ],
+    },
+    {
+      'If you live in Disctrict of Columbia, What is the capital of your state?': [
+        'D.C. is not a state',
+        'Washington DC',
+        'Arlington',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What are the two major political parties in the United States?': [
+        'Democratic and Republican',
+        'Green Party and Liberals',
+        'Liberatarian and People\'s Party',
+        'Liberals and Conservatives',
+      ],
+    },
+    {
+      'What is the political party of the President now?': [
+        'Republican',
+        'Democratic',
+        'Liberatarian',
+        'Green Party',
+      ],
+    },
+    {
+      'What is the name of the Speaker of the House of Representatives now?': [
+        'Paul Ryan',
+        'Bernie Sanders',
+        'Nancy Pelosy',
+        'Don\'t know',
+      ],
+    },
+    {
+      'There are four amendments to the Constitution about who can vote. What is one of them?': [
+        'Citizens eighteen (18) and older can vote',
+        'Anybody can vote',
+        'You don\'t have to be 18 to vote',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Which one of the following is responsibility that is only for United States citizens?': [
+        'Serve on a jury',
+        'Be respectful',
+        'Be courteous',
+        'Study law',
+      ],
+    },
+    {
+      'Which one is not rights of everyone living in the United States?': [
+        'freedom of respect',
+        'freedom of speech',
+        'freedom of assembly',
+        'freedom to petition the government',
+      ],
+    },
+    {
+      'What do we show loyalty to when we say the Pledge of Allegiance?': [
+        'the United States',
+        'the House',
+        'the President',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What is one promise you make when you become a United States citizen?': [
+        'give up loyalty to other countries',
+        'be respectful to the others',
+        'pass the citizenship test',
+        'Don\'t know',
+      ],
+    },
+    {
+      'How old do citizens have to be to vote for President?': [
+        '18',
+        '21',
+        '16',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What are ways that Americans can participate in their democracy?': [
+        'call Senators and Representatives',
+        'joining the military',
+        'work in federal office',
+        'Don\'t know',
+      ],
+    },
+    {
+      'When is the last day you can send in federal income tax forms?': [
+        'April 15',
+        'December 31',
+        'November 8',
+        'January 31',
+      ],
+    },
+    {
+      'When must all men register for the Selective Service?': [
+        'at age 18',
+        'at age 21',
+        'at age 16',
+        'at age 23',
+      ],
+    },
+    {
+      'What is one reason colonists came to America?': [
+        'freedom',
+        'to fight war',
+        'to vote',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Who lived in America before the Europeans arrived?': [
+        'Native Americans',
+        'Mexicans',
+        'Asians',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What group of people was taken to America and sold as slaves?': [
+        'Africans',
+        'Mexicans',
+        'Asians',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Why did the colonists fight the British?': [
+        'because of high taxes',
+        'because they were communist',
+        'because they didn\'t like British',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Who wrote the Declaration of Independence?': [
+        'Thomas Jefferson',
+        'James Madison',
+        'Benjamin Franklin',
+        'Alexander Hamilton',
+      ],
+    },
+    {
+      'When was the Declaration of Independence adopted?': [
+        'July 4, 1776',
+        'July 4, 1786',
+        'July 4, 1774',
+        'July 4, 1766',
+      ],
+    },
+    {
+      'There were 13 original states. What are three states?': [
+        'New Hampshire, Massachusetts and Connecticut',
+        'Connecticut, New York and Florida',
+        'Virginia, North Carolina, Maine',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What happened at the Constitutional Convention?': [
+        'The Constitution was written.',
+        'Declared independence',
+        'Declared war',
+        'Don\'t know',
+      ],
+    },
+    {
+      'When was the Constitution written?': [
+        '1787',
+        '1764',
+        '1779',
+        '1786',
+      ],
+    },
+    {
+      'The Federalist Papers supported the passage of the U.S. Constitution. Who is one of the writers?': [
+        'James Madison',
+        'Benjamin Franklin',
+        'Thomas Jefferson',
+        'Susan B Anthony',
+      ],
+    },
+    {
+      'What is one thing Benjamin Franklin is famous for?': [
+        'US Diplomat',
+        'US President',
+        'Free Slavery',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Who is the “Father of Our Country”?': [
+        'George Washington',
+        'Benjamin Franklin',
+        'Thomas Jefferson',
+        'Abraham Lincoln',
+      ],
+    },
+    {
+      'Who was the first President?': [
+        'George Washington',
+        'Benjamin Franklin',
+        'Abraham Lincoln',
+        'Thomas Jefferson',
+      ],
+    },
+    {
+      'What territory did the United States buy from France in 1803?': [
+        'Louisiana',
+        'Guam',
+        'Puero Rico',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What war was fought by the United States in the 1800s?': [
+        'Mexican-American War',
+        'World War I',
+        'Vietnam War',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What was the U.S. war between the North and the South?': [
+        'the Civil War',
+        'Mexican-American War',
+        'War of 1812',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What was one problem that led to the Civil War?': [
+        'Slavery',
+        'Colony',
+        'Religion',
+        'British',
+      ],
+    },
+    {
+      'What was one important thing that Abraham Lincoln did?': [
+        'Freed the slaves',
+        'Fought for civil rights',
+        'Fought for Women\'s rights',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What did the Emancipation Proclamation do?': [
+        'freed the slaves',
+        'Printed the money',
+        'Declared Independence',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What did Susan B. Anthony do?': [
+        'fought for civil rights',
+        'Freed the slaves',
+        'Donated money',
+        'Ran for office',
+      ],
+    },
+    {
+      'Which war was not fought by the United States in the 1900s?': [
+        'World War I',
+        'the Civil War',
+        'Mexican-American War',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Who was President during World War I?': [
+        'Woodrow Wilson',
+        'George Washington',
+        'Abraham Lincoln',
+        'Franklin Roosevelt',
+      ],
+    },
+    {
+      'Who was President during the Great Depression and World War II?': [
+        'Franklin Roosevelt',
+        'George Washington',
+        'Abraham Lincoln',
+        'Woodrow Wilson',
+      ],
+    },
+    {
+      'Who did the United States fight in World War II?': [
+        'Japan Germany and Italy',
+        'Sweden, Japan and Spain',
+        'Germany, Poland and Norway',
+        'Italy, Sweden and Germany',
+      ],
+    },
+    {
+      'Before he was President, Eisenhower was a general. What war was he in?': [
+        'World War II',
+        'World War I',
+        'Civil War',
+        'Mexican-American War',
+      ],
+    },
+    {
+      'During the Cold War, what was the main concern of the United States?': [
+        'Communism',
+        'Slavery',
+        'Colony',
+        'Government Shutdown',
+      ],
+    },
+    {
+      'What movement tried to end racial discrimination?': [
+        'Civil rights',
+        'Human rights',
+        'Women\'s rights',
+        'Gender equality',
+      ],
+    },
+    {
+      'What did Martin Luther King, Jr. do?': [
+        'fought for human rights',
+        'fought for civil rights',
+        'fought for gender equality',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What major event happened on September 11, 2001, in the United States?': [
+        'Terrorist attack',
+        'Civil War',
+        'Mass Shooting',
+        'Don\'t know',
+      ],
+    },
+    {
+      'Which one of the following is a American Indian tribe in the United States?': [
+        'Navajo',
+        'Celts',
+        'Zawi Chemi',
+        'Slavs',
+      ],
+    },
+    {
+      'What is one of the two longest rivers in the United States?': [
+        'Mississippi River',
+        'Colorado River',
+        'Ohio River',
+        'Rio Grande River',
+      ],
+    },
+    {
+      'What ocean is on the West Coast of the United States?': [
+        'Pacific Ocean',
+        'Atlantic Ocean',
+        'Arctic Ocean',
+        'Southern Ocean',
+      ],
+    },
+    {
+      'Which one of the following is not U.S. territory?': [
+        'San Juan',
+        'Puerto Rico',
+        'Guam',
+        'American Samoa',
+      ],
+    },
+    {
+      'Which of the following states borders Canada?': [
+        'New York',
+        'California',
+        'Texas',
+        'Oregon',
+      ],
+    },
+    {
+      'Which of the following states borders Mexico?': [
+        'California',
+        'New York',
+        'Florida',
+        'Minnesota',
+      ],
+    },
+    {
+      'What is the capital of the United States?': [
+        'Washington DC',
+        'New York',
+        'Seattle',
+        'Philadelphia',
+      ],
+    },
+    {
+      'Where is the Statue of Liberty?': [
+        'New York Harbor',
+        'Buffalo',
+        'Queens',
+        'Long Island',
+      ],
+    },
+    {
+      'Why does the flag have 13 stripes?': [
+        'Represents 13 colonies',
+        'Because 13 stripes looks good',
+        'Only 13 stripes would fit',
+        'Because 13 is good number',
+      ],
+    },
+    {
+      'Why does the flag have 50 stars?': [
+        'Each star represents each state',
+        'Because 50 people came to USA',
+        'Because only 50 stars would fit',
+        'Don\'t know',
+      ],
+    },
+    {
+      'What is the name of the national anthem?': [
+        'Star Spangled Banner',
+        'America the beautiful',
+        'God Bless the USA',
+        'My Country Tis of Thee',
+      ],
+    },
+    {
+      'When do we celebrate Independence Day?': [
+        'July 4',
+        'September 1',
+        'August 15',
+        'February 14',
+      ],
+    },
+    {
+      'Which of the following is not national U.S. holidays?': [
+        'Valentine\'s Day',
+        'Christmas',
+        'Labor Day',
+        'Thanksgiving',
+      ],
+    },
+  ],
+}
